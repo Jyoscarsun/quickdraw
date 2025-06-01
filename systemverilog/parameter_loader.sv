69e7da0edd117594cb7b5c6c62e7e7ac3bc705b2
Oscar <156103596+Jyoscarsun@users.noreply.github.com>
2025-06-01 12:08:38 -0400

    Update parameter_loader.sv

--- systemverilog/parameter_loader.sv
+++ systemverilog/parameter_loader.sv
@@ -3,10 +3,12 @@ module conv1_w(
 );
     // Declare memory array
     logic signed [7:0] memory [0:143];
+
     // Load memory contents from a file during initialization
     initial begin
         $readmemh("parameters/conv1_w.mif", memory);
     end
+
     // Map the linear memory to the 3D weights structure
     initial begin
         for (int f = 0; f < 16; f++) begin
@@ -19,32 +21,32 @@ module conv1_w(
     end
 endmodule
 
-
-
 module conv1_b(
     output logic signed [31:0] biases[0:15]
 );
     logic signed [31:0] memory [0:15];
-    inital begin
+
+    initial begin
         $readmemh("parameters/conv1_b.mif", memory);
     end
-    inital begin
+
+    initial begin
         for(int i = 0; i < 16; i++)begin
             biases[i] = memory[i];
         end
     end
 endmodule
 
-
-
 module conv2_w(
-    output logic signed [7:0] weights[0:31][0:15][0:2][0:2].
+    output logic signed [7:0] weights[0:31][0:15][0:2][0:2]
 );
     //Hardware specific, Altera/intel FPGA has M10K memory blocks
     (* ramstyle = "M10K" *) logic signed [7:0] memory [0:4607];
+
     initial begin
         $readmemh("parameters/conv2_w.mif", memory);
     end
+
     genvar f, c, i, j;
     generate
         for(f = 0; f < 32; f++) begin: filter_loop
@@ -59,34 +61,33 @@ module conv2_w(
     endgenerate
 endmodule
 
-
-
 module conv2_b(
-    output logic signed [31:0] biases[0:31],
+    output logic signed [31:0] biases[0:31]
 );  
     logic signed [31:0] memory [0:31];
-    inital begin
+
+    initial begin
         $readmemh("parameters/conv2_b.mif", memory);
     end
-    inital begin
-        for(int i = 0; i < 31; i++)begin
+
+    initial begin
+        for(int i = 0; i < 32; i++)begin
             biases[i] = memory[i];
         end
     end
 endmodule
 
-
-
 module fc1_w(
-    output logic signed [7:0] weights[0:127][0:1567],
+    output logic signed [7:0] weights[0:127][0:1567]
 );
     //number of parallel memory banks (TUNE based on available resources)
     localparam NUM_BANKS = 16;
     localparam WEIGHTS_PER_BANK = 200704/NUM_BANKS;
+
     //declare memory banks with M10K directive (Altera/Intel FPGA specific)
     genvar bank;
     generate
-        for(bank = 0; bank < NUM_BANKS, bank++)begin: memory_banks
+        for(bank = 0; bank < NUM_BANKS; bank++)begin: memory_banks
             //each abnk stores some weights
             (* ramstyle = "M10K" *)
             logic signed[7:0] mem_bank[0:WEIGHTS_PER_BANK-1];
@@ -99,6 +100,7 @@ module fc1_w(
             end
         end
     endgenerate
+
     //map from memory banks to output weight structure
     genvar neuron, input_idx;
     generate
@@ -114,15 +116,15 @@ module fc1_w(
     endgenerate
 endmodule
 
-
-
 module fc1_b(
     output logic signed [31:0] biases[0:127]
 );
     logic signed [31:0] memory [0:127];
+
     initial begin
         $readmemh("parameters/fc1_b.mif", memory);
     end
+
     initial begin
         for (int i = 0; i < 128; i++) begin
             biases[i] = memory[i];
@@ -130,15 +132,15 @@ module fc1_b(
     end
 endmodule
 
-
-
 module fc2_w(
     output logic signed [7:0] weights[0:9][0:127]
 );
     (* ramstyle = "M10K" *) logic signed [7:0] memory [0:1279];
+
     initial begin
         $readmemh("parameters/fc2_w.mif", memory);
     end
+    
     genvar class_idx, input_idx;
     generate
         for (class_idx = 0; class_idx < 10; class_idx++) begin : classes
@@ -150,15 +152,15 @@ module fc2_w(
     endgenerate
 endmodule
 
-
-
 module fc2_b(
     output logic signed [31:0] biases[0:9]
 );
     logic signed [31:0] memory [0:9];
+
     initial begin
         $readmemh("parameters/fc2_b.mif", memory);
     end
+
     initial begin
         for (int i = 0; i < 10; i++) begin
             biases[i] = memory[i];
